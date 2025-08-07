01ab8d6dfb6bede33ac79b86c5c9dcda4a21edb0
Julio Caesar <leo.caesar@live.com>
2025-08-08 04:31:25 +0800

    feat: wallet connector button

---
+++ web/src/providers/providers.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { type ReactNode } from "react";
+import {
+  RainbowKitProvider,
+  getDefaultConfig,
+  getDefaultWallets,
+} from "@rainbow-me/rainbowkit";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { appChains } from "@/lib/chains";
+
+const queryClient = new QueryClient();
+
+
+const { wallets } = getDefaultWallets();
+
+if (!process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID) {
+  throw new Error(
+    "Environment variable NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID must be defined"
+  );
+}
+
+const walletConnectProjectId =
+  process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
+
+
+export const wagmiConfig = getDefaultConfig({
+  appName: "Argenta",
+  projectId: walletConnectProjectId,
+  wallets,
+  chains: appChains,
+  ssr: true,
+});
+
+export function AppProviders({ children }: { children: ReactNode }) {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+
