a0570d8c140cc6c9bfb26831a667797105e76ed8
Julio Caesar <leo.caesar@live.com>
2025-08-09 05:21:27 +0800

    code cleanup

--- web/src/providers/providers.tsx
+++ web/src/providers/providers.tsx
@@ -12,26 +12,6 @@ import { wagmiConfig } from "@/wagmi";
 
 const queryClient = new QueryClient();
 
-// const { wallets } = getDefaultWallets();
-
-// if (!process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID) {
-//   throw new Error(
-//     "Environment variable NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID must be defined"
-//   );
-// }
-
-// const walletConnectProjectId =
-//   process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
-
-
-// export const wagmiConfig = getDefaultConfig({
-//   appName: "Argenta",
-//   projectId: walletConnectProjectId,
-//   wallets,
-//   chains: appChains,
-//   ssr: true,
-// });
-
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
     <WagmiProvider config={wagmiConfig}>
