d9eeb3a6cf025269eba60cf8bf77bec6a15bee86
Julio Caesar <leo.caesar@live.com>
2025-08-12 04:44:17 +0800

    code cleanup

--- web/src/hooks/index.ts
+++ web/src/hooks/index.ts
@@ -1,6 +1,4 @@
 // Balance hooks
-export { useTokenBalance } from "./useTokenBalance";
-export { useVaultBalance } from "./useVaultBalance";
 export { useChainBalances } from "./useChainBalances";
 
 // Portfolio hooks
@@ -10,13 +8,6 @@ export { usePortfolioTotals } from "./usePortfolioTotals";
 export { useInputValidation } from "./useInputValidation";
 export type { UseInputValidationProps } from "./useInputValidation";
 export { useOperationValidation } from "./useOperationValidation";
-
-// Transaction hooks
-export { useTokenApproval } from "./useTokenApproval";
-export type {
-  UseTokenApprovalParams,
-  UseTokenApprovalReturn,
-} from "./useTokenApproval";
 
 // Batch operation hooks
 export { useBatchDepositValidation } from "./useBatchDepositValidation";
